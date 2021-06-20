db927db696273e330fcecf41aa90ca00d70d257f
Len <leonard.vaughn@gmail.com>
2021-06-20 15:59:16 -0700

    Enabled subnet as part of 1st successful deployment.

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,7 @@ provider "google" {
 # VPC
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
+  auto_create_subnetworks = "true"
 }
 
 # Subnet
